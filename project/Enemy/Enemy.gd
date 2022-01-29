a09b152df0c18c4e71184a73fa454f74138b9588
the-alex-g <alex.gestwicki@gmail.com>
2022-01-29 11:24:38 -0500

    Add enemy that tracks player
    There is a weird issue with the drawing that I don't understand
    but we can figure it out later

---
+++ project/Enemy/Enemy.gd
@@ -0,0 +1,39 @@
+extends KinematicBody2D
+# signals
+
+# enums
+
+# constants
+
+# exported variables
+export var speed := 100.0
+
+# public variables
+var target: KinematicBody2D setget _set_target
+
+# private variables
+var _direction := Vector2.RIGHT
+
+# onready variables
+
+
+func _physics_process(delta: float)->void:
+	var angle_to_target = get_angle_to(target.get_global_transform().origin)
+	rotation += angle_to_target
+	
+	var collision := move_and_collide((_direction * speed * delta).rotated(rotation))
+	
+#	if collision != null:
+#		if collision.collider == target:
+#			target.damage()
+
+
+func _draw()->void:
+	draw_circle($CollisionShape2D.position, $CollisionShape2D.shape.radius, Color.red)
+
+
+func _set_target(new_target: KinematicBody2D)->void:
+	target = new_target
+	var target_id: int = target.player_index
+	
+	collision_mask = 1 if target_id == 0 else 2
