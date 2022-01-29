a75e72c5b1bf21a39c6f90212965afe7a18f4585
Paul Gestwicki <pvgestwicki@bsu.edu>
2022-01-29 16:59:55 -0500

    Animate title screen shaders

--- project/UI/StartMenu.gd
+++ project/UI/StartMenu.gd
@@ -1,10 +1,17 @@
 extends Control
 
 onready var _play_button := $PlayButton
-
+onready var _shadery_things := [$Title, $Background, $Label]
 
 func _ready():
 	_play_button.grab_focus()
+
+
+func _process(_delta):
+	var seconds := OS.get_ticks_msec() / 1000.0
+	var x := range_lerp(sin(seconds), -1, 1, 412, 612)
+	for control in _shadery_things:
+		control.material.set_shader_param("barrier_x", x)
 
 
 func _on_PlayButton_pressed():
